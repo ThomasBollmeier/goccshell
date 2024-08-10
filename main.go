ad7c2e53659ca2a7d6b2ddbf9d0d0d3229326146
Thomas Bollmeier <developer@thomas-bollmeier.de>
2024-08-10 15:48:26 +0200

    Finish step 1

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"github.com/chzyer/readline"
+	"os"
+	"os/exec"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Coding Challenges Shell")
+	input, err := prompt()
+	if err != nil {
+		panic(err)
+	}
+	handleInput(input)
+}
+
+func handleInput(input string) bool {
+	parts := strings.Split(input, " ")
+	cmd := exec.Command(parts[0], parts[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+
+	return err == nil
+}
+
+func prompt() (string, error) {
+	command, err := readline.Line("ccsh> ")
+	if err != nil {
+		return "", err
+	}
+	command = strings.TrimRight(command, " \n")
+	return command, nil
 }
